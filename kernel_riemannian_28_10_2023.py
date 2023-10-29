87e3fb77a7e358ce7f33cd491cad115e9a604f53
shubha96ngi <shubha.96n.gi@gmail.com>
2023-10-29 15:16:06 +0100

    Update kernel_riemannian_28_10_2023.py

--- kernel_riemannian_28_10_2023.py
+++ kernel_riemannian_28_10_2023.py
@@ -1,4 +1,13 @@
 from pymanopt.manifolds import  SymmetricPositiveDefinite
+from tensorflow_riemopt import manifolds
+E = manifolds.Euclidean()
+SPD = manifolds.SPDAffineInvariant()
+manifold = manifolds.Product((SPD, (3,3)), (E, (2,)))
+from geoopt.manifolds import Euclidean, ProductManifold, SymmetricPositiveDefinite
+SPD = SymmetricPositiveDefinite()
+E = Euclidean()
+manifold =  ProductManifold((SPD,(12,320)), (E,(12)))
+
 def create_cost_and_derivates(manifold, matrix):
     euclidean_gradient = None
     @pymanopt.function.autograd(manifold)
